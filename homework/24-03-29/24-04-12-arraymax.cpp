53f553f52a358e354b3637e368888aa3176db0c9
GiantGKL <2494127609@qq.com>
2024-04-13 13:13:50 +0800

    24-04-12

--- homework/24-03-29/24-04-12-arraymax.cpp
+++ homework/24-03-29/24-04-12-arraymax.cpp
@@ -8,7 +8,7 @@ void Array_max::set_value(){
 }
 void Array_max::max_value(){
     int i;
-    max=array[0];  //wzk
+    max=array[0];
     for (i=1;i<10;i++)
         if (array[i]>max)
             max=array[i];
